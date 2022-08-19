049051c62185b025081a37ba446968c159b4fddf
xoraus <imsafay@gmail.com>
2022-08-19 13:23:30 +0530

    initial + v5 and Navbar

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './components/Navbar';
+
+
+function App() {
+  return (
+  <>
+  <Navbar title = "textUtils" aboutText = "About" />
+  </>
+);
+}
+
+export default App;
